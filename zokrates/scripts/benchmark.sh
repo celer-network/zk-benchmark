77c7011c339fac4d28b8a8b556ea0b4d236dcb7c
aric <aric@celer.network>
2023-02-07 13:06:38 +0800

    rm out and r1cs to save storage

--- zokrates/scripts/benchmark.sh
+++ zokrates/scripts/benchmark.sh
@@ -5,12 +5,14 @@ do
 n=$[2 ** $i]
 cd zok/$n
 echo "benchmark size: "$[$n*64]
-#/usr/bin/time -f "%M %e" sh ../../scripts/init.sh > log
-#/usr/bin/time -f "%M %e"  sh ../../scripts/run.sh $n >> log
-#/usr/bin/time -f "%M %e"  sh ../../scripts/verify.sh >> log
+#/usr/bin/time -f "%M %e" bash ../../scripts/init.sh > log
+#/usr/bin/time -f "%M %e"  bash ../../scripts/run.sh $n >> log
+#/usr/bin/time -f "%M %e"  bash ../../scripts/verify.sh >> log
 gtime -f "%M %e" sh ../../scripts/init.sh > log
 gtime -f "%M %e"  sh ../../scripts/run.sh $n >> log
 gtime -f "%M %e"  sh ../../scripts/verify.sh >> log
+rm out
+rm out.r1cs
 echo ""
 cd ../..
 done
