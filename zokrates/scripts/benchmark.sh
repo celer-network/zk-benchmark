a90cc71dbb5ccb955432a5eee32c00a176002abc
aric <aric@celer.network>
2023-02-07 11:05:28 +0800

    refine zok

--- zokrates/scripts/benchmark.sh
+++ zokrates/scripts/benchmark.sh
@@ -5,9 +5,9 @@ do
 n=$[2 ** $i]
 cd zok/$n
 echo "benchmark size: "$[$n*64]
-#/usr/bin/time -f "%M %e" sh ../init.sh > log
-#/usr/bin/time -f "%M %e"  sh ../run.sh $n >> log
-#/usr/bin/time -f "%M %e"  sh ../verify.sh >> log
+#/usr/bin/time -f "%M %e" sh ../../scripts/init.sh > log
+#/usr/bin/time -f "%M %e"  sh ../../scripts/run.sh $n >> log
+#/usr/bin/time -f "%M %e"  sh ../../scripts/verify.sh >> log
 gtime -f "%M %e" sh ../../scripts/init.sh > log
 gtime -f "%M %e"  sh ../../scripts/run.sh $n >> log
 gtime -f "%M %e"  sh ../../scripts/verify.sh >> log
